22a5181da29c857af4d3b7aa71965245218474d5
meteoseeker <64303562+meteoseeker@users.noreply.github.com>
2023-03-13 20:28:12 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'ac_possible',
-  github: 'meteoseeker',
-  linkedin: '',
+  //twitter: 'ac_possible',
+  //github: 'meteoseeker',
+  //linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
